d403eb87a5c175a5bb8c813bd21be25c96a6316e
Attila Bakos <attila.bakos@dartke.hu>
2019-12-04 19:57:02 +0100

    Kód rendezés

--- src/pages/Favourites.jsx
+++ src/pages/Favourites.jsx
@@ -23,18 +23,29 @@ const Favourites = (props) => {
             pageTitle='Kedvencek'
             pageId={2}>
 
+
             <FoodCard
                 fullwidth
-                gradient
                 cover={cover1}
-                title="Teszt címsor ami nagyon hosszú és nem fér ki"
+                title="FoodCard komponens minta #1"
                 text="A Lorem Ipsum egy egyszerû szövegrészlete, szövegutánzata a betûszedõ és nyomdaiparnak. A Lorem Ipsum az 1500-as évek óta standard szövegrészletként szolgált az iparban; mikor egy ismeretlen nyomdász összeállította a betûkészletét és egy példa-könyvet vagy szöveget nyomott papírra, ezt használta." />
 
             <FoodCard
                 fullwidth
                 gradient
                 cover={cover2}
-                title="Teszt címsor ami nagyon hosszú és nem fér ki"
+                title="FoodCard komponens minta #2"
+                text="A Lorem Ipsum egy egyszerû szövegrészlete, szövegutánzata a betûszedõ és nyomdaiparnak. A Lorem Ipsum az 1500-as évek óta standard szövegrészletként szolgált az iparban; mikor egy ismeretlen nyomdász összeállította a betûkészletét és egy példa-könyvet vagy szöveget nyomott papírra, ezt használta." />
+
+            <FoodCard
+                gradient
+                cover={cover1}
+                title="FoodCard komponens minta #3"
+                text="A Lorem Ipsum egy egyszerû szövegrészlete, szövegutánzata a betûszedõ és nyomdaiparnak. A Lorem Ipsum az 1500-as évek óta standard szövegrészletként szolgált az iparban; mikor egy ismeretlen nyomdász összeállította a betûkészletét és egy példa-könyvet vagy szöveget nyomott papírra, ezt használta." />
+
+            <FoodCard
+                cover={cover2}
+                title="FoodCard komponens minta #4"
                 text="A Lorem Ipsum egy egyszerû szövegrészlete, szövegutánzata a betûszedõ és nyomdaiparnak. A Lorem Ipsum az 1500-as évek óta standard szövegrészletként szolgált az iparban; mikor egy ismeretlen nyomdász összeállította a betûkészletét és egy példa-könyvet vagy szöveget nyomott papírra, ezt használta." />
         </MainLayout>
     );
